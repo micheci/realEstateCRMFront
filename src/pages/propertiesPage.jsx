6833b2114520cd61b399e0092dc52a3d5a73988e
micheci <michecimartinez@gmail.com>
2025-02-03 17:22:14 -0600

    added button to navigate to add new property

--- src/pages/propertiesPage.jsx
+++ src/pages/propertiesPage.jsx
@@ -1,6 +1,9 @@
 import usePropertyStore from "../store/propertyStore";
 import PropertyCard from "../components/propertyCard"; // Importing PropertyCard
 import { useEffect } from "react";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import Box from "@mui/material/Box";
 
 const PropertiesPage = () => {
   const store = usePropertyStore();
@@ -11,9 +14,11 @@ const PropertiesPage = () => {
   if (loading) return <div>Loading properties...</div>;
   if (error) return <div>Error: {error}</div>;
 
-  console.log(properties, "LR");
   return (
     <div>
+      <Fab color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
       <h2 className="text-xl font-semibold mb-4">Manage Your Properties</h2>
       <p>Here you can manage all your listed properties.</p>
 
